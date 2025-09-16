9e2ae75dc3e499ab943b3d11567657accf342489
abu7667 <fijiim3@gmail.com>
2025-09-16 15:49:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+Array = ["non, banan, mashina"]
+console.log(Array + ":n harifi ishtirok etgan sozlar")
+
+Array = ["asal, olma, aziza, gul"]
+console.log(Array + ":n harifi ishtirok etmagan sozlar")
